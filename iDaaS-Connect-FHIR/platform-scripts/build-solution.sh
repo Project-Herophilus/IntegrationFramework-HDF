9bf762b8ad0eef8524106c283a94f4de5db0dea4
Alan Scott <alscott@redhat.com>
2021-07-04 10:23:21 -0500

    Enhancement of Accelerator Add-ons for better usage - Moved FHIR Samples

--- iDaaS-Connect-FHIR/platform-scripts/build-solution.sh
+++
@@ -1,5 +0,0 @@
-echo "Building iDAAS - Connect FHIR"
-cd ../
-
-mvn clean install
-echo "Maven Build Completed"
--- iDaaS-Connect-FHIR/platform-scripts/stop-solution.sh
+++
@@ -1,7 +0,0 @@
-echo "Stoping Kafka running in background"
-cd kafka/non-windows
-./stop_kafka.sh
-echo "Stoping iDaas - Connect HL7"
-cd ../../..
-cd target
-kill $(cat ./bin/shutdown.pid)
