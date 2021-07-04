8923d3a8127ba3dda34a716c828ada7f07d4f60d
Alan Scott <alscott@redhat.com>
2021-07-04 10:17:32 -0500

    Enhancement of Accelerator Add-ons for better usage

--- iDaaS-Connect-FHIR/platform-scripts/kafka/non-windows/stop_kafka.sh
+++
@@ -1,8 +0,0 @@
-kafkaDir=$HOME'/OpenSourceTech/kafka_2.13-2.6.0'
-echo "Directory: "$kafkaDir
-cd $kafkaDir
-
-bin/kafka-server-stop.sh config/server.properties
-bin/zookeeper-server-stop.sh config/zookeeper.properties
-
-#rm -rf /tmp/kafka-logs /tmp/zookeeper
