e51562b8ad48e8c311fe761a15abedf3587d45b8
Jonathan Myer <jonathan.myer@ibm.com>
2021-07-13 16:15:01 -0400

    removed docker file

--- build-script.sh
+++ build-script.sh
@@ -1,35 +1,108 @@
 #!/bin/bash
-echo -e "Build environment variables:"
-echo "REGISTRY_URL=${REGISTRY_URL}"
-echo "REGISTRY_NAMESPACE=${REGISTRY_NAMESPACE}"
-echo "IMAGE_NAME=${IMAGE_NAME}"
-echo "BUILD_NUMBER=${BUILD_NUMBER}"
+#set -x
 
-# Learn more about the available environment variables at:
-# https://cloud.ibm.com/docs/services/ContinuousDelivery?topic=ContinuousDelivery-deliverypipeline_environment#deliverypipeline_environment
+# Use this script as a starting point to create your own deployment.yml
 
-# To review or change build options use:
-# ibmcloud cr build --help
-
-echo -e "Checking for Dockerfile at the repository root"
-if [ -f Dockerfile ]; then 
-   echo "Dockerfile found"
-else
-    echo "Dockerfile not found"
-    exit 1
+# Make sure the cluster is running and get the ip_address
+ip_addr=$(ibmcloud ks workers --cluster $PIPELINE_KUBERNETES_CLUSTER_NAME | grep normal | awk '{ print $2 }')
+if [ -z $ip_addr ]; then
+  echo "$PIPELINE_KUBERNETES_CLUSTER_NAME not created or workers not ready"
+  exit 1
 fi
 
-#ensure docker and buildkit are present if not already in current pipeline-base-image
-which buildctl > /dev/null || (curl -fsSL https://github.com/moby/buildkit/releases/download/v0.8.0/buildkit-v0.8.0.linux-amd64.tar.gz | tar zxf - --strip-components=1 -C /usr/bin bin/buildctl)
-which docker > /dev/null || (curl -fsSL https://download.docker.com/linux/static/stable/x86_64/docker-19.03.9.tgz | tar zxf - --strip-components=1 -C /usr/bin docker/docker)
+# Initialize script variables
+NAME="idaas-connect-fhir"
+IMAGE="$PIPELINE_IMAGE_URL"
+if [ -z IMAGE ]; then
+  echo "$IMAGE not set. If using $PIPELINE_IMAGE_URL this variable is only configured when a "Container Registry" build job is used as the stage input."
+  exit 1
+fi
+PORT=$(ibmcloud cr image-inspect $IMAGE --format '{{ range $key,$value := .ContainerConfig.ExposedPorts }} {{ $key }} {{ "" }} {{end}}' | sed -E 's/^[^0-9]*([0-9]+).*$/\1/')
+if [ -z "$PORT" ]; then
+    PORT=5000
+    echo "Port not found in Dockerfile, using $PORT"
+fi
 
-FULL_IMAGE_NAME=$REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME
+echo ""
+echo "Deploy environment variables:"
+echo "NAME=$NAME"
+echo "IMAGE=$IMAGE"
+echo "PORT=$PORT"
+echo ""
 
-echo -e "Building container image"
-set -x
-ibmcloud cr login
-buildctl build --frontend dockerfile.v0 --local context=. --local dockerfile=. \
-  --output type=image,name=${FULL_IMAGE_NAME}:$BUILD_NUMBER,push=true \
-  --export-cache type=registry,ref=${FULL_IMAGE_NAME}:buildcache \
-  --import-cache type=registry,ref=${FULL_IMAGE_NAME}:buildcache
-set +x
+DEPLOYMENT_FILE="deployment.yml"
+echo "Creating deployment file $DEPLOYMENT_FILE"
+
+# Build the deployment file
+DEPLOYMENT=$(cat <<EOF''
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: $NAME
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: $NAME
+  template:
+    metadata:
+      labels:
+        app: $NAME
+    spec:
+      containers:
+      - name: $NAME
+        image: $IMAGE
+        imagePullPolicy: IfNotPresent
+        ports:
+        - containerPort: $PORT
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: $NAME
+  labels:
+    app: $NAME
+spec:
+  type: NodePort
+  ports:
+    - port: $PORT
+  selector:
+    app: $NAME
+EOF
+)
+
+# Substitute the variables
+echo "$DEPLOYMENT" > $DEPLOYMENT_FILE
+sed -i 's/$NAME/'"$NAME"'/g' $DEPLOYMENT_FILE
+sed -i 's=$IMAGE='"$IMAGE"'=g' $DEPLOYMENT_FILE
+sed -i 's/$PORT/'"$PORT"'/g' $DEPLOYMENT_FILE
+
+# Show the file that is about to be executed
+echo ""
+echo "DEPLOYING USING MANIFEST:"
+echo "cat $DEPLOYMENT_FILE"
+cat $DEPLOYMENT_FILE
+echo ""
+
+# Execute the file
+echo "KUBERNETES COMMAND:"
+echo "kubectl apply -f $DEPLOYMENT_FILE"
+kubectl apply -f $DEPLOYMENT_FILE --n=anthem-cdp-poc
+echo ""
+
+echo ""
+echo "DEPLOYED SERVICE:"
+kubectl describe services $NAME
+echo ""
+echo "DEPLOYED PODS:"
+kubectl describe pods --selector app=$NAME
+echo ""
+
+# Show the IP address and the PORT of the running app
+port=$(kubectl get services | grep "$NAME " | sed 's/.*:\([0-9]*\).*/\1/g')
+echo "RUNNING APPLICATION:"
+echo "URL=http://$ip_addr"
+echo "PORT=$port"
+echo ""
+echo "$NAME running at: http://$ip_addr:$port"
+
