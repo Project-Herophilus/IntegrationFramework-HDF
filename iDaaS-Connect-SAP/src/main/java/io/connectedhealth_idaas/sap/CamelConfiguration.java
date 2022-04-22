db41c8ff506a5a7a42e0c1136dfd16932c0d04bf
Alan Scott <64438147+balanscott@users.noreply.github.com>
2022-04-21 20:45:43 -0500

    Created iDaaS-Connect-SAP. Also, corrected several README.md files from the various iDaaS-Connect modules. Also, corrected the server.port definitions for the various iDaaS-Connect modules. (#13)
    Co-authored-by: redhat-healthcare-chiefarchitect <alscott@redhat.com>

--- iDaaS-Connect-SAP/src/main/java/io/connectedhealth_idaas/sap/CamelConfiguration.java
+++ iDaaS-Connect-SAP/src/main/java/io/connectedhealth_idaas/sap/CamelConfiguration.java
@@ -14,7 +14,7 @@
  * permissions and limitations under the License.
  *
  */
-package io.connectedhealth_idaas.cloud;
+package io.connectedhealth_idaas.sap;
 
 //import javax.jms.ConnectionFactory;
 
@@ -38,22 +38,6 @@ import org.apache.camel.component.jackson.JacksonDataFormat;
 /*
  *  Kafka implementation based on https://camel.apache.org/components/latest/kafka-component.html
  *  JDBC implementation based on https://camel.apache.org/components/latest/dataformats/hl7-dataformat.html
- *  JPA implementayion based on https://camel.apache.org/components/latest/jpa-component.html
- *  File implementation based on https://camel.apache.org/components/latest/file-component.html
- *  FileWatch implementation based on https://camel.apache.org/components/latest/file-watch-component.html
- *  FTP/SFTP and FTPS implementations based on https://camel.apache.org/components/latest/ftp-component.html
- *  JMS implementation based on https://camel.apache.org/components/latest/jms-component.html
- *  JT400 (AS/400) implementation based on https://camel.apache.org/components/latest/jt400-component.html
- *  HTTP implementation based on https://camel.apache.org/components/latest/http-component.html
- *  HDFS implementation based on https://camel.apache.org/components/latest/hdfs-component.html
- *  jBPMN implementation based on https://camel.apache.org/components/latest/jbpm-component.html
- *  MongoDB implementation based on https://camel.apache.org/components/latest/mongodb-component.html
- *  RabbitMQ implementation based on https://camel.apache.org/components/latest/rabbitmq-component.html
- *  There are lots of third party implementations to support cloud storage from Amazon AC2, Box and so forth
- *  There are lots of third party implementations to support cloud for Amazon Cloud Services
- *  Awaiting update to 3.1 for functionality
- *  Apache Kudu implementation
- *  REST API implementations
  */
 @Component
 public class CamelConfiguration extends RouteBuilder {
@@ -185,26 +169,6 @@ public class CamelConfiguration extends RouteBuilder {
     *  Kafka Implementation for implementing Third Party FHIR Server direct connection
     */
 
-    // Sample Using Kafka Topic
-    // FHIR: Adverse Events
-    from(getKafkaTopicUri("fhirsvr_adverseevent"))
-        .routeId("AdverseEvent-MiddleTier")
-        // Auditing
-        .setProperty("processingtype").constant("data")
-        .setProperty("appname").constant("iDAAS-ConnectClinical-IndustryStd")
-        .setProperty("industrystd").constant("FHIR")
-        .setProperty("messagetrigger").constant("AdverseEvent")
-        .setProperty("component").simple("${routeId}")
-        .setProperty("camelID").simple("${camelId}")
-        .setProperty("exchangeID").simple("${exchangeId}")
-        .setProperty("internalMsgID").simple("${id}")
-        .setProperty("bodyData").simple("${body}")
-        .setProperty("processname").constant("MTier")
-        .setProperty("auditdetails").constant("Adverse Event to Enterprise By Data Type middle tier")
-        //.wireTap("direct:auditing")
-        // Enterprise Message By Type
-        .convertBodyTo(String.class).to(getKafkaTopicUri("ent_fhirsvr_adverseevent"))
-    ;
 
   }
 }
