b974a4d0f5a12bbcab8588eacc9775e82fdbf072
Alan Scott <balanscott@outlook.com>
2022-10-21 02:05:15 -0500

    Updates:
    - Enhanced application-dev.properties and CmsInteroperabilityRouteBuilder.java to support industry centric use cases specific
    to Davinci

--- iDaaS-Connect-CMS-Interoperability/src/main/java/io/connectedhealth_idaas/cmsinteroperability/CmsInteroperabilityRouteBuilder.java
+++ iDaaS-Connect-CMS-Interoperability/src/main/java/io/connectedhealth_idaas/cmsinteroperability/CmsInteroperabilityRouteBuilder.java
@@ -17,9 +17,10 @@ public class EdiRouteBuilder extends RouteBuilder {
   public static final String PIXAddUpdate_INBD_ROUTE_ID = "pixaddupdate-inbound";
   public static final String ProvideReqDocs_INBD_ROUTE_ID = "providerreqdocs-inbound";
   public static final String Davinci_ClinDex_INBD_ROUTE_ID = "davinci-clindex-inbound";
-  public static final String EDI835_INBD_ROUTE_ID = "edi-835-inbound";
-  public static final String EDI837_INBD_ROUTE_ID = "edi-837-inbound";
-
+  public static final String PriorAuth_INBD_ROUTE_ID = "priorauth-inbound";
+  public static final String DEQM_INBD_ROUTE_ID = "deqm-inbound";
+  public static final String PDEX_INBD_ROUTE_ID = "pdex-inbound";
+  public static final String EPDX_INBD_ROUTE_ID = "epdx-inbound";
   @Override
   public void configure() throws Exception {
 
@@ -51,7 +52,7 @@ public class EdiRouteBuilder extends RouteBuilder {
             .to("log:" + PIXAddUpdate_INBD_ROUTE_ID + "?showAll=true")
             .log("${exchangeId} fully processed")
             .to("micrometer:counter:numProcessedFiles")
-            .to("kafka:{{idaas.xds.topic.name}}?brokers={{idaas.kafka.brokers}}");
+            .to("kafka:{{idaas.pixaddupdate.topic.name}}?brokers={{idaas.kafka.brokers}}");
             //perform needed Actions to persist PIX Documents to a XDS Repository/DataStore
     // Provide Required Docs
     from("rest:post:/idaas/providereqdocs")
@@ -59,7 +60,7 @@ public class EdiRouteBuilder extends RouteBuilder {
             .to("log:" + ProvideReqDocs_INBD_ROUTE_ID + "?showAll=true")
             .log("${exchangeId} fully processed")
             .to("micrometer:counter:numProcessedFiles")
-            .to("kafka:{{idaas.xds.topic.name}}?brokers={{idaas.kafka.brokers}}");
+            .to("kafka:{{idaas.providerreqdocs.topic.name}}?brokers={{idaas.kafka.brokers}}");
             //perform needed Actions to persist Provide Docs to a XDS Repository/DataStore
 
     // Davinci
@@ -71,19 +72,56 @@ public class EdiRouteBuilder extends RouteBuilder {
     // DEQM (Data Exchange Quality Measures) :  https://build.fhir.org/ig/HL7/davinci-deqm/
     // CDEX (Clinical Data Exchange)         :  http://hl7.org/fhir/us/davinci-cdex/
     // PDEX (Payer Data Exchange)            :  https://hl7.org/fhir/us/davinci-pdex/
+    // EPDX (Performing Lab Reporting)       :  http://hl7.org/fhir/us/core/StructureDefinition-us-core-diagnosticreport-lab.html
 
     // Prior Auth
+    from("rest:post:/idaas/priorauth")
+            .routeId(PriorAuth_INBD_ROUTE_ID)
+            .to("log:" + PriorAuth_INBD_ROUTE_ID + "?showAll=true")
+            .log("${exchangeId} fully processed")
+            .to("micrometer:counter:numProcessedFiles")
+            .to("kafka:{{idaas.priorauth.topic.name}}?brokers={{idaas.kafka.brokers}}");
+            //perform needed Actions to built the correct response
+            //respond back with content
 
     // Data Exchange Quality Metrics
-
+    from("rest:post:/idaas/deqm")
+            .routeId(DEQM_INBD_ROUTE_ID)
+            .to("log:" + DEQM_INBD_ROUTE_ID + "?showAll=true")
+            .log("${exchangeId} fully processed")
+            .to("micrometer:counter:numProcessedFiles")
+            .to("kafka:{{idaas.deqm.topic.name}}?brokers={{idaas.kafka.brokers}}");
+            //perform needed Actions to built the correct response
+            //respond back with content
     // Clinical Data Exchange
-
+    from("rest:post:/idaas/deqm")
+            .routeId(DEQM_INBD_ROUTE_ID)
+            .to("log:" + DEQM_INBD_ROUTE_ID + "?showAll=true")
+            .log("${exchangeId} fully processed")
+            .to("micrometer:counter:numProcessedFiles")
+            .to("kafka:{{idaas.deqm.topic.name}}?brokers={{idaas.kafka.brokers}}");
+            //perform needed Actions to built the correct response
+            //respond back with content
     // Payer Data Exchange
-
-
+    from("rest:post:/idaas/pdex")
+            .routeId(PDEX_INBD_ROUTE_ID)
+            .to("log:" + PDEX_INBD_ROUTE_ID + "?showAll=true")
+            .log("${exchangeId} fully processed")
+            .to("micrometer:counter:numProcessedFiles")
+            .to("kafka:{{idaas.pdex.topic.name}}?brokers={{idaas.kafka.brokers}}");
+            //perform needed Actions to built the correct response
+            //respond back with content
     // Pt Data Exchange
 
-    // Perfcorming Lab Reporting
+    // Performing Lab Reporting
+    from("rest:post:/idaas/epdx")
+            .routeId(EPDX_INBD_ROUTE_ID)
+            .to("log:" + EPDX_INBD_ROUTE_ID + "?showAll=true")
+            .log("${exchangeId} fully processed")
+            .to("micrometer:counter:numProcessedFiles")
+            .to("kafka:{{idaas.epdx.topic.name}}?brokers={{idaas.kafka.brokers}}");
+            //perform needed Actions to built the correct response
+            //respond back with content
 
 
   }
