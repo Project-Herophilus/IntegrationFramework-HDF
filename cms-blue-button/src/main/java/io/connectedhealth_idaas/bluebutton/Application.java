f3f3feb0cc1d454757e6d93e067aae89ba416cac
Guilherme Camposo <guilherme.camposo@me.com>
2022-11-04 14:47:48 -0400

    fixing build

--- cms-blue-button/src/main/java/io/connectedhealth_idaas/bluebutton/Application.java
+++ cms-blue-button/src/main/java/io/connectedhealth_idaas/bluebutton/Application.java
@@ -21,7 +21,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ConfigProperties.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
