2f25e2dc0fc85efa542d5848eaa986051604743a
Guilherme Camposo <guilherme.camposo@me.com>
2023-01-11 13:23:48 -0500

    fixing build and adding specific version to fhir

--- cloud/src/main/java/io/connectedhealth_idaas/cloud/Application.java
+++ cloud/src/main/java/io/connectedhealth_idaas/cloud/Application.java
@@ -18,10 +18,8 @@ package io.connectedhealth_idaas.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ConfigProperties.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
